99a053ef9d82bd6d026f6c27af7db062ecd74879
Eshaan Bansal <eshaan7bansal@gmail.com>
2024-04-15 15:47:12 +0530

    format using prettier

--- bridge/src/index.ts
+++ bridge/src/index.ts
@@ -13,52 +13,50 @@ const abiCoder = AbiCoder.defaultAbiCoder();
 const operator = new Wallet(process.env.PRIVATE_KEY as string);
 
 const signMessage = async (
-    wallet: Wallet,
-    schema: ActionSchema,
-    payload: AllowedInputTypes
+  wallet: Wallet,
+  schema: ActionSchema,
+  payload: AllowedInputTypes
 ) => {
-    const signature = await wallet.signTypedData(
-        schema.domain,
-        schema.EIP712TypedData.types,
-        payload
-    );
-    return signature;
+  const signature = await wallet.signTypedData(
+    schema.domain,
+    schema.EIP712TypedData.types,
+    payload
+  );
+  return signature;
 };
 
-
 async function main() {
-    const rollup = await MicroRollup({
-        config: stackrConfig,
-        actionSchemas: [MintTokenSchema],
-        stateMachines: [machine]
-    })
-    await rollup.init();
+  const rollup = await MicroRollup({
+    config: stackrConfig,
+    actionSchemas: [MintTokenSchema],
+    stateMachines: [machine],
+  });
+  await rollup.init();
 
-    Bridge.init(rollup, {
-        handlers: {
-            'BRIDGE_ETH': async (args) => {
-                const [_to, _amount] = abiCoder.decode(['address', 'uint'], args.data);
+  Bridge.init(rollup, {
+    handlers: {
+      BRIDGE_ETH: async (args) => {
+        const [_to, _amount] = abiCoder.decode(["address", "uint"], args.data);
 
-                const inputs = {
-                    address: _to,
-                    amount: Number(_amount)
-                }
+        const inputs = {
+          address: _to,
+          amount: Number(_amount),
+        };
 
-                const signature = await signMessage(operator, MintTokenSchema, inputs);
-                const action = MintTokenSchema.actionFrom({
-                    inputs,
-                    signature,
-                    msgSender: operator.address
-                })
+        const signature = await signMessage(operator, MintTokenSchema, inputs);
+        const action = MintTokenSchema.actionFrom({
+          inputs,
+          signature,
+          msgSender: operator.address,
+        });
 
-                return {
-                    transitionName: 'mintToken',
-                    action: action
-                }
-            }
-        }
-    })
-
+        return {
+          transitionName: "mintToken",
+          action: action,
+        };
+      },
+    },
+  });
 }
 
 main();
--- bridge/src/stackr/machine.ts
+++ bridge/src/stackr/machine.ts
@@ -4,28 +4,26 @@ import { solidityPackedKeccak256 } from "ethers";
 import * as genesisState from "../../genesis-state.json";
 import { transitions } from "./transitions";
 
-
 type Balances = {
-    address: string;
-    balance: number;
+  address: string;
+  balance: number;
 }[];
 
-
 export class BridgeState extends State<Balances> {
-    constructor(state: Balances) {
-        super(state);
-    }
+  constructor(state: Balances) {
+    super(state);
+  }
 
-    getRootHash() {
-        return solidityPackedKeccak256(["string"], [JSON.stringify(this.state)]);
-    }
+  getRootHash() {
+    return solidityPackedKeccak256(["string"], [JSON.stringify(this.state)]);
+  }
 }
 
 const machine = new StateMachine({
-    id: "bridge",
-    stateClass: BridgeState,
-    initialState: genesisState.state as Balances,
-    on: transitions,
+  id: "bridge",
+  stateClass: BridgeState,
+  initialState: genesisState.state as Balances,
+  on: transitions,
 });
 
 export { machine };
