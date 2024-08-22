e6df87a731306e60dd3641744da53496ca80e16c
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-08-22 19:09:28 +0530

    fix: empty example

---
+++ empty/src/stackr/machine.ts
@@ -0,0 +1,5 @@
+import { StateMachine } from "@stackr/sdk/machine";
+import genesisState from "../../genesis-state.json";
+
+// Define and export the State Machine
+// this will require State class, Transitions, and genesis state
---
+++ empty/src/stackr/schemas.ts
@@ -0,0 +1,3 @@
+import { ActionSchema, SolidityType } from "@stackr/sdk";
+
+// define and export the schema for your action
